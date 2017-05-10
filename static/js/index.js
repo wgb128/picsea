5f87ab4ebae5a8c4cc05f304002613b86b2838d7
cheesejmj <750593303@qq.com>
2017-05-10 16:38:24 +0800

    addfiles

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-	// 1
-	$(".seltype").on("change",function(){
-		$(".ChooseSeltype span").text($(this).children('option:selected').text());
+
+	// choose
+	$(".chs-head>div").click( function(){
+		var index=$(this).index();
+		$(this).addClass("select").siblings().removeClass("select");
+		$(".chs-list").addClass("show").eq(index).siblings(".chs-list").removeClass("show");
 	});
 
-	// 2
-	$(".selstyle").on("change",function(){
-		$(".ChooseSelstyle span").text($(this).children('option:selected').text());
+
+	// 1
+	$(".seltype").on("change",function(){
+		$(".chs-field p span").text($(this).children('option:selected').text());
 	});
+
+	// 回到顶部
+	$("#returnTop").click(function () {
+        var speed=200;
+        $('body,html').animate({ scrollTop: 0 }, speed);
+        return false;
+ });
+
 })
